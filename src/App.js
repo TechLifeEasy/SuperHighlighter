72db2fc5355a458bb18cf65f3bb7a4e300904fe2
zeel-codder <zeelprajapati321@gmail.com>
2021-10-30 19:45:14 +0530

    add firbase for CRUD On Notes

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 import './App.css';
 import { useEffect ,useState} from 'react';
 import { Logout, SingUp ,UserAuthState,SingIn} from './Api/user';
+import {GetNoteWebLinkUser,GetNoteList,CreateUserWebData,UpdateUserWebData} from './Api/notes';
 
 function App() {
 
@@ -28,6 +29,21 @@ function App() {
       <button onClick={() => SingIn({email:"zeelprajapati321@gmail.com",password:"123212"})} > SingIn </button>
 
       <button onClick={Logout} > Log out </button>
+
+
+      <button onClick={()=>GetNoteWebLinkUser({webLink:'https://www.freecodecamp.org/learn/back-end-development-and-apis/#managing-packages-with-npm'})} > GetNotes </button>
+      
+      
+      <button onClick={()=>GetNoteList('https://www.freecodecamp.org/learn/back-end-development-and-apis/#managing-packages-with-npm')} > GetList </button>
+      
+      <button onClick={()=>CreateUserWebData('https://stackoverflow.com/questions/50692218/how-can-i-get-specific-document-data-from-firestore-querysnapshot')} > Create Connection </button>
+
+      <button onClick={()=>UpdateUserWebData('https://www.freecodecamp.org/learn/back-end-development-and-apis/#managing-packages-with-npm','zeel',true)} >
+         Update Connection
+          </button>
+      <button onClick={()=>UpdateUserWebData('https://www.freecodecamp.org/learn/back-end-development-and-apis/#managing-packages-with-npm','zeel',false)} >
+         Remove Connection
+          </button>
     </div>
   );
 }
--- src/script/index.js
+++ src/script/index.js
@@ -1,7 +1,6 @@
 console.log('call')
 
 
-
 window.chrome.runtime.onMessage.addListener(
     function(message, messageSender, sendResponse) {
     console.log(message,messageSender);
