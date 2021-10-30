8ac18a72f171d00646466500f74f2fdf5cb21b9c
zeel-codder <zeelprajapati321@gmail.com>
2021-10-30 16:08:20 +0530

    User Auth is Done in firebase

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
 
 import './App.css';
+import { useEffect ,useState} from 'react';
+import { Logout, SingUp ,UserAuthState,SingIn} from './Api/user';
 
 function App() {
+
+  const [user,setUser]=useState(null);
+
+  useEffect(()=>{
+    UserAuthState(setUser)
+  },[])
+
+
   return (
     <div className="App">
-     Notes 👱
+
+      Notes 👱
+
+      {
+        user!=null && <div>{user.displayName}</div>
+      }
+
+
+
+      <button onClick={() => SingUp({email:"zeelprajapati321@gmail.com",password:"123212",displayName:"zeel"})} > SingUp </button>
+
+      <button onClick={() => SingIn({email:"zeelprajapati321@gmail.com",password:"123212"})} > SingIn </button>
+
+      <button onClick={Logout} > Log out </button>
     </div>
   );
 }
--- src/script/index.js
+++ src/script/index.js
@@ -2,8 +2,8 @@ console.log('call')
 
 
 
-// window.chrome.runtime.onMessage.addListener(
-//     function(message, messageSender, sendResponse) {
-//     console.log(message,messageSender);
-//     }
-// );
+window.chrome.runtime.onMessage.addListener(
+    function(message, messageSender, sendResponse) {
+    console.log(message,messageSender);
+    }
+);
